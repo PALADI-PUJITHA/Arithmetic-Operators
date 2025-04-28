ab9112905a396eb3183a0254cf96110b56661a2b
PALADI-PUJITHA <paladipujitha@gmail.com>
2025-04-28 15:31:34 +0530

    Add files via upload

---
+++ ArithmeticOperators.java
@@ -0,0 +1,21 @@
+public class ArithmeticOperators{
+	public static void main(String args[]){
+		int a=7;
+		int b=2;
+		System.out.println("a value:"+a);
+		System.out.println("b value:"+b);
+		System.out.println("a+b="+(a+b));
+		System.out.println("a value:"+a);
+		System.out.println("b value:"+b);
+		System.out.println("a-b="+(a-b));
+		System.out.println("a value:"+a);
+		System.out.println("b value:"+b);
+		System.out.println("a*b="+(a*b));
+		System.out.println("a value:"+a);
+		System.out.println("b value:"+b);
+		System.out.println("a/b="+(a/b));
+		System.out.println("a value:"+a);
+		System.out.println("b value:"+b);
+		System.out.println("a%b="+(a%b));
+	}
+}
